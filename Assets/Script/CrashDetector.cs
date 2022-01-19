5dee65e0a1d5267e038bfed9536d5ce5262a1470
Emanuele Parlascino <emapage95@gmail.com>
2022-01-19 14:27:46 +0000

    added particle effects and scene management

--- Assets/Script/CrashDetector.cs
+++ Assets/Script/CrashDetector.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float crashReloadTime = 1f;
+    [SerializeField] ParticleSystem crashEffect;
   void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("You bonked your head"); 
+            crashEffect.Play();
+            Invoke("ReloadScene", crashReloadTime);
         }
     }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
 }
